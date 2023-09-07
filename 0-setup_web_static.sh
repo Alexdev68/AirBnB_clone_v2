7033378d1534ffe1b9b2b172eb244154b72acd00
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-09-07 05:21:08 -0700

    Fixed issue

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,17 +4,17 @@
 sudo apt-get update
 sudo apt-get -y install nginx
 
-mkdir -p /data/web_static/releases/test/
-touch /data/web_static/releases/test/index.html
-mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo mkdir -p /data/web_static/shared/
 
 cat <<EOF > /data/web_static/releases/test/index.html
 <html>
-	<head>
-	</head>
-	<body>
-		Holberton School
-	</body>
+        <head>
+        </head>
+        <body>
+                Holberton School
+        </body>
 </html>
 EOF
 
@@ -22,5 +22,5 @@ ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
 
-printf "\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tindex index.html;\n\t}" >> /etc/nginx/sites-available/default
+sudo sed -i "/listen \[::\]:80 default_server;/a\\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tindex index.html;\n\t}" /etc/nginx/sites-available/default
 sudo service nginx restart
