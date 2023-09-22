8ac3f1d8497b0e691280126bc6022bd9b9c61491
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-09-22 11:32:59 -0700

    New file

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""This script starts a Flask web application and performs some actions."""
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def hello():
+    """This function returns `Hello HBNB`"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """This function returns `HBNB`"""
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def C_text(text):
+    """This function returns C with the text and changes all the underscores
+    to spaces.
+    """
+    space = text.replace("_", " ")
+    fmt = 'C ' + space
+
+    return fmt
+
+
+@app.route("/python/")
+@app.route("/python/<text>")
+def py_text(text="is cool"):
+    """This function returns Python with whatever the text is and changes
+    all the underscores to spaces.
+    """
+    space = text.replace("_", " ")
+    fmt = "Python " + space
+
+    return fmt
+
+
+@app.route("/number/<n>")
+def num(n):
+    """This function returns a number only if it is an integer."""
+    if not isinstance(int(n), int):
+        return ""
+    else:
+        return f"{n} is a number"
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
