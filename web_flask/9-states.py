4c7581b6dac33fda693ade0d178f562ab02c40f6
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-09-24 06:15:38 -0700

    Devugging

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -14,13 +14,16 @@ def teardown(anything):
 
 
 @app.route("/states", strict_slashes=False)
+def state():
+    '''lists states'''
+    ob = storage.all(State)
+    return render_template('7-states_list.html', states=ob)
+
+
 @app.route("/states/<id>", strict_slashes=False)
-def states_id(id=None):
+def states_id(id):
     '''lists cities in states based on id'''
     ob = storage.all(State)
-
-    if id is None:
-        return render_template('9-states.html', states=ob)
 
     for st in ob.values():
         if st.id == id:
